63ec29e6992d2ef1885a22ac18657242ffb8597d
Matt Mauro <matt.mauro@gmail.com>
2016-11-14 15:20:45 -0500

    Initial Commit

---
+++ scrollImageGalleries/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  scrollImageGalleries
+//
+//  Created by Matthew Mauro on 2016-11-14.
+//  Copyright © 2016 Matthew Mauro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
